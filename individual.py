87466880cfda6f2fcf115135f9677f5937ec14df
Yajahira Jauregui Diaz <113789120+yjhr21@users.noreply.github.com>
2024-11-05 21:49:52 -0500

    Funcion añadir Paso
    lógica para agregar un paso a la ruta del individuo

--- individual.py
+++ individual.py
@@ -1,6 +1,7 @@
 from maze import Maze
 
 class Individual:
+   
    def __init__(self, maze, path=None): #inicializar las variables 
    self.path = path if path else []
    self.fitness = 0
@@ -9,3 +10,20 @@ class Individual:
    self.venom_count = 0 
    self.visited_positions = set(self.path)
    self.stagnant_steps = 0
+   
+   #lógica para agregar un paso a la ruta del individuo
+   def add_step(self, step, maze):
+      if self._is_cardinal_move(self.get_last_position(), step) and maze.is_valid_move(step) and not self.goal_reached:
+         if step in self.visited_positions:
+               self.venom_count += 5
+               self.stagnant_steps += 5
+         else:
+               self.path.append(step)
+               self.visited_positions.add(step)
+               
+         if step == self.goal:
+               self.goal_reached = True
+               self.venom_count = 0
+               self.stagnant_steps = 0
+               
+         maze.add_venom(step, amount=3)  
